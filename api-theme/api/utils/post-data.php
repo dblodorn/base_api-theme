89abde76dd9d1c5ecd8e0e79e40029b1099b9336
dblodorn <dain@db13.us>
2018-10-09 11:01:36 -0700

    all fields for post collection

--- api-theme/api/utils/post-data.php
+++ api-theme/api/utils/post-data.php
@@ -1,4 +1,19 @@
 <?php
+  function return_link_list($p) {
+    $links = array();
+    if ( have_rows( 'link_list', $p->id ) ) :
+      while ( have_rows( 'link_list', $p->id ) ) : the_row();
+        $links[] = array(
+          'url' => get_sub_field('url'),
+          'title' => get_sub_field('title'),
+        );
+      endwhile;
+    else :
+      $links = false;
+    endif;
+    return $links;
+  }
+  
   function post_data($post) {
     $template = get_post_meta( $post->ID, '_wp_page_template', true );
     $template_name = return_template($template);
@@ -15,6 +30,7 @@
       'short_description' => get_field('short_description', $post->ID),
       'description' => get_field('description', $post->ID),
       'container_width' => get_field('top_container_width', $post->ID),
+      'link_list' => return_link_list($post),
       'content' => template_data($template_name, $post),
       'taxonomies' => taxonomy_data($post),
     );
